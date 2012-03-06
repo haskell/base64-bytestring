3d697fce865583c2a26d74a11e3717d718f86136
Bryan O'Sullivan <bos@serpentine.com>
2012-03-05 21:48:44 -0800

    Add more benchmarks

--- benchmarks/BM.hs
+++ benchmarks/BM.hs
@@ -1,8 +1,29 @@
-import Data.ByteString.Base64
-import Data.ByteString.Char8 (pack)
+{-# LANGUAGE OverloadedStrings #-}
+
 import Criterion.Main
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Base64 as B
+import qualified Data.ByteString.Base64 as U
+
+sized name orig =
+    bgroup name [
+      bgroup "normal" [
+        bench "decode" $ whnf B.decode enc
+      , bench "decodeLenient" $ whnf B.decodeLenient enc
+      , bench "encode" $ whnf B.encode orig
+      ]
+    , bgroup "url" [
+        bench "decode" $ whnf U.decode enc
+      , bench "decodeLenient" $ whnf U.decodeLenient enc
+      , bench "encode" $ whnf U.encode orig
+      ]
+    ]
+  where enc = U.encode orig
 
 main :: IO ()
-main = defaultMain
-   [ bench "encode" $ whnf encode $ pack "qwerty"
-   ]
+main = defaultMain [
+         sized "small" input
+       , sized "medium" (B.concat (replicate 100 input))
+       , sized "large" (B.concat (replicate 10000 input))
+       ]
+  where input = "abcdABCD0123[];'"
