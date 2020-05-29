204d2aee35e799e801af0331dacb36d59a5c8e99
Emily Pillmore <emilypi@cohomolo.gy>
2020-05-29 14:16:35 -0400

    fix messages

--- Data/ByteString/Base64/Internal.hs
+++ Data/ByteString/Base64/Internal.hs
@@ -134,9 +134,8 @@ decodeWithTable padding decodeFP bs =
    case padding of
      Padded
        | r == 0 -> unsafePerformIO $ go bs
-       | r == 2 -> Left "Base64-encoded bytestring required to be padded"
-       | r == 3 -> Left "Base64-encoded bytestring has invalid padding"
-       | otherwise -> Left "Base64-encoded bytestring has invalid size"
+       | r == 1 -> Left "Base64-encoded bytestring has invalid size"
+       | otherwise -> Left "Base64-encoded bytestring is unpadded or has invalid padding"
      Don'tCare
        | r == 0 -> unsafePerformIO $ go bs
        | r == 2 -> unsafePerformIO $ go (B.append bs (B.replicate 2 0x3d))
