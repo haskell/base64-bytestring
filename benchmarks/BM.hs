249c4fe6eefa26e3453603c6f7702cdedc6a12c2
Emily Pillmore <emilypi@cohomolo.gy>
2020-11-09 15:09:03 -0500

    fix regression

--- benchmarks/BM.hs
+++ benchmarks/BM.hs
@@ -1,72 +1,81 @@
-{-# LANGUAGE CPP               #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
+module Main
+( main
+) where
 
+
+import Criterion
 import Criterion.Main
-import qualified Data.ByteString.Base64 as B
-import qualified Data.ByteString.Base64.Lazy as L
-import qualified Data.ByteString.Base64.URL as U
-import qualified Data.ByteString.Base64.URL.Lazy as LU
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy as L
 
-#if !MIN_VERSION_bytestring(0,10,0)
-import Control.DeepSeq (NFData(rnf))
-import qualified Data.ByteString.Lazy.Internal as L
-#endif
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base64 as B64
 
-strict :: String -> B.ByteString -> Benchmark
-strict name orig =
-    bgroup name [
-      bgroup "normal" [
-        bench "decode" $ whnf B.decode enc
-      , bench "decodeLenient" $ whnf B.decodeLenient enc
-      , bench "encode" $ whnf B.encode orig
-      ]
-    , bgroup "url" [
-        bench "decode" $ whnf U.decode enc
-      , bench "decodeLenient" $ whnf U.decodeLenient enc
-      , bench "encode" $ whnf U.encode orig
-      , bench "encodeUnpadded" $ whnf U.encodeUnpadded orig
-      ]
-    ]
-  where enc = U.encode orig
-
-#if !MIN_VERSION_bytestring(0,10,0)
-instance NFData L.ByteString where
-    rnf L.Empty        = ()
-    rnf (L.Chunk _ ps) = rnf ps
-#endif
-
-lazy :: String -> L.ByteString -> Benchmark
-lazy name orig =
-    bgroup name [
-      bgroup "normal" [
-        bench "decode" $ nf L.decode enc
-      , bench "decodeLenient" $ nf L.decodeLenient enc
-      , bench "encode" $ nf L.encode orig
-      ]
-    , bgroup "url" [
-        bench "decode" $ nf LU.decode enc
-      , bench "decodeLenient" $ nf LU.decodeLenient enc
-      , bench "encode" $ nf LU.encode orig
-      , bench "encodeUnpadded" $ whnf LU.encodeUnpadded orig
-      ]
-    ]
-  where enc = L.encode orig
 
 main :: IO ()
-main = defaultMain [
-         bgroup "lazy" [
-           lazy "small" (L.fromChunks [input])
-         , lazy "medium" (L.concat . replicate 16 . L.fromChunks . (:[]) .
-                          B.concat $ replicate 8 input)
-         , lazy "large" (L.concat . replicate 1280 . L.fromChunks . (:[]) .
-                          B.concat $ replicate 8 input)
-         ]
-       , bgroup "strict" [
-           strict "small" input
-         , strict "medium" (B.concat (replicate 128 input))
-         , strict "large" (B.concat (replicate 10240 input))
-         ]
-       ]
-  where input = "abcdABCD0123[];'"
+main =
+  defaultMain
+    [ env bs $ \ ~(bs25,bs100,bs1k,bs10k,bs100k,bs1mm) ->
+      bgroup "encode"
+      [ bgroup "25"
+        [ bench "base64" $ whnf B64.encode bs25
+        ]
+      , bgroup "100"
+        [ bench "base64" $ whnf B64.encode bs100
+        ]
+      , bgroup "1k"
+        [ bench "base64" $ whnf B64.encode bs1k
+        ]
+      , bgroup "10k"
+        [ bench "base64" $ whnf B64.encode bs10k
+        ]
+      , bgroup "100k"
+        [ bench "base64" $ whnf B64.encode bs100k
+        ]
+      , bgroup "1mm"
+        [ bench "base64" $ whnf B64.encode bs1mm
+        ]
+      ]
+    , env bs' $ \ ~(bs25,bs100,bs1k,bs10k,bs100k,bs1mm) ->
+      bgroup "decode"
+      [ bgroup "25"
+        [ bench "base64" $ whnf B64.decode bs25
+        ]
+      , bgroup "100"
+        [ bench "base64" $ whnf B64.decode bs100
+        ]
+      , bgroup "1k"
+        [ bench "base64" $ whnf B64.decode bs1k
+        ]
+      , bgroup "10k"
+        [ bench "base64" $ whnf B64.decode bs10k
+        ]
+      , bgroup "100k"
+        [ bench "base64" $ whnf B64.decode bs100k
+        ]
+      , bgroup "1mm"
+        [ bench "base64" $ whnf B64.decode bs1mm
+        ]
+      ]
+    ]
+  where
+    bss :: BS.ByteString
+    bss = "ab%de^ghi*"
+
+    bs = do
+      let !a = BS.concat $ replicate 3 bss
+          !b = BS.concat $ replicate 10 bss
+          !c = BS.concat $ replicate 100 bss
+          !d = BS.concat $ replicate 1000 bss
+          !e = BS.concat $ replicate 10000 bss
+          !f = BS.concat $ replicate 100000 bss
+      return (a,b,c,d,e,f)
+
+    bs' = do
+      let !a = B64.encode (BS.concat $ replicate 3 bss)
+          !b = B64.encode (BS.concat $ replicate 10 bss)
+          !c = B64.encode (BS.concat $ replicate 100 bss)
+          !d = B64.encode (BS.concat $ replicate 1000 bss)
+          !e = B64.encode (BS.concat $ replicate 10000 bss)
+          !f = B64.encode (BS.concat $ replicate 100000 bss)
+      return (a,b,c,d,e,f)
